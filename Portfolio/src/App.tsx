d1a68098f6e8b25b760533d88e77c69803052157
JASPER <jasjasper101@gmail.com>
2023-02-14 15:28:27 -0800

    Implement React Template + Tailwind configs

---
+++ Portfolio/src/App.tsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import styles from './input.css'
+import './App.css'
+
+function App() {
+
+  return (
+    <div className="bg-amber-700">
+      My Portfolio
+    </div>
+  )
+}
+
+export default App
