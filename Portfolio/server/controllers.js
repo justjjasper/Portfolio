e70015e1c07c4be058a21161826ca66d4ec5618d
JASPER <jasjasper101@gmail.com>
2023-09-21 21:25:18 -0700

    Implement controller templates

---
+++ Portfolio/server/controllers.js
@@ -0,0 +1,3 @@
+export const postContact = async (req, res) => {
+  res.status(200).send('confirmed working');
+};
--- Portfolio/server/index.js
+++ Portfolio/server/index.js
@@ -1,10 +1,13 @@
 import express from 'express'
 import cors from 'cors'
+import  { postContact }  from './controllers.js';
 
 var app = express();
 const port = 3000;
 app.use(express.json());
 app.use(cors());
+
+app.post('/contact', postContact)
 
 app.listen(port, () => {
   console.log(`Server is now listening to porter ${port}`)
