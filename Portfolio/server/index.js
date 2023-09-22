481def4078844b6637fd0822596969092cfcc0e8
JASPER <jasjasper101@gmail.com>
2023-09-22 14:34:02 -0700

    change vite.config.js settings and ports env

--- Portfolio/server/index.js
+++ Portfolio/server/index.js
@@ -5,12 +5,11 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 var app = express();
-const port = 3000;
 app.use(express.json());
 app.use(cors());
 
 app.post('/contact', postContact)
 
-app.listen(port, () => {
+app.listen(process.env.PORT, () => {
   console.log(`Server is now listening to porter ${process.env.PORT}`)
 })
