1c893ff7b808352ba76de38ce085643ad9e6cb0e
JASPER <jasjasper101@gmail.com>
2024-01-09 15:11:49 -0800

    cange

--- Portfolio/vite.config.ts
+++ Portfolio/vite.config.ts
@@ -1,11 +1,17 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path'
+import fs from 'fs'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
+    https: {
+      key: '/home/ubuntu/key',
+      cert: '/home/ubuntu/cert',
+    },
     host: true,
-    port: 80
+    port: 443
   }
 })
