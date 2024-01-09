2c15948cbe56aa4e434d1aeb878d639114fc11a4
JASPER <jasjasper101@gmail.com>
2024-01-09 15:01:50 -0800

    server configs

--- Portfolio/vite.config.ts
+++ Portfolio/vite.config.ts
@@ -1,15 +1,17 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path'
+import fs from 'fs'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
     https: {
-      key: '/home/ubuntu/key',
-      cert: '/home/ubuntu/cert'
+      key: fs.readFileSync(path.resolve(__dirname, 'key')),
+      cert: fs.readFileSync(path.resolve(__dirname, 'cert')),
     },
     host: true,
-    port: 80
+    port: 443
   }
 })
