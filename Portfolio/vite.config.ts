eb13e5a3837681bb5e862415a955b2da1cf6b741
JASPER <jasjasper101@gmail.com>
2024-01-09 14:41:42 -0800

    add https

--- Portfolio/vite.config.ts
+++ Portfolio/vite.config.ts
@@ -1,11 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-
+import KEY
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
+    https: {
+      key: '/home/ubuntu/key',
+      cert: '/home/ubuntu/cert'
+    },
     host: true,
-    port: 443
+    port: 80
   }
 })
