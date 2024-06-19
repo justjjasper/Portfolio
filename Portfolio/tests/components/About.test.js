9dd1201f2fa0ea9159c8cac5026514d4208ab8b6
JASPER <jasjasper101@gmail.com>
2024-06-19 12:39:51 -0700

    change url server

--- Portfolio/tests/components/About.test.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import About from '../../src/components/About';
-
-test('renders "about." text', () => {
-  const { getByText } = render(<About />);
-  const aboutText = getByText(/about\./i); // Case-insensitive search for "about."
-  expect(aboutText).toBeInTheDocument();
-});
